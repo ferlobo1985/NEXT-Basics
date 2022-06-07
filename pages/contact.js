968de85f6e96091a78e47e20af15fc8010d7d9d9
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-06-07 04:32:57 -0300

    05-Navigation

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,19 @@
+import { useRouter } from 'next/router'
+
 const ContactPage = () => {
+    const router = useRouter();
+
+    const redirectUser = () => {
+      router.push('/');
+    }
+
+
     return(
       <>
         <h1>Contact</h1>
+        <button onClick={redirectUser}>
+          Go to home
+        </button>
       </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import Link from 'next/link'
+
 const HomePage = () => {
   return(
     <>
       <h1>Honey I am home</h1>
+      <ul>
+        <li>
+          <Link href="/users">Users</Link>
+        </li>
+        <li>
+          <Link href="/users/111">User 111</Link>
+        </li>
+        <li>
+          <Link href="/wheels/4/red/round">Wheel</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
     </>
   )
 }
