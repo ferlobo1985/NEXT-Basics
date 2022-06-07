57227bef49bda4f80c085db0e2ce77fa7fc1c209
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-06-07 05:10:02 -0300

    aa

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-  <>
-      <Head>
-          <title>Awesome app</title>
-      </Head>
-    <Component {...pageProps} />
-  </>
+    <>
+        <Head>
+            <title>Awesome app</title>
+        </Head>
+      <Component {...pageProps} />
+    </>
   )
 }
 
